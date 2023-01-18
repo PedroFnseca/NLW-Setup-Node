9445e533384d73ee9bfb265a700bd94d31224c69
pedrofnseca <pedropereira.pp264@gmail.com>
2023-01-17 22:33:27 -0300

    chore: config cors

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors"
 
 const app = Fastify()
-const prisma = new PrismaClient();
+
+app.register(cors)
+
+const prisma = new PrismaClient()
 
 app.get('/', async () => {
-    const havits = await prisma.habit.findMany({
-         where: {
-            title: {
-                startsWith: 'Ser'
-            }
-         }
-    })
+    const habits = await prisma.habit.findMany( )
 
-    return havits
+    return habits
 })
 
 app.listen({
